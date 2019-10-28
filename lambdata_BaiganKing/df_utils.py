fc288cdc60da672e4355d721b8960cd728d76669
Andrew <BaiganKing@users.noreply.github.com>
2019-10-28 16:25:08 -0600

    Fixed function error

--- lambdata_BaiganKing/df_utils.py
+++ lambdata_BaiganKing/df_utils.py
@@ -11,18 +11,18 @@ def date(x, col):
     if x.df[col].dtype != 'datetime64[ns]':
         x.df[col] = pd.to_datetime(x.df[col])
 
-     name = x.df[col].name
-        x.df[f'{name}_year'] = x.df[col].dt.year
-        x.df[f'{name}_month'] = x.df[col].dt.month
-        x.df[f'{name}_day'] = x.df[col].dt.day
+    name = x.df[col].name
+    x.df[f'{name}_year'] = x.df[col].dt.year
+    x.df[f'{name}_month'] = x.df[col].dt.month
+    x.df[f'{name}_day'] = x.df[col].dt.day
 
-        x.df[f'{name}_month'] = x.df[f'{name}_month'].map({
-            1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
-            6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
-            11: 'November', 12: 'December'
-            })
+    x.df[f'{name}_month'] = x.df[f'{name}_month'].map({
+        1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
+        6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
+        11: 'November', 12: 'December'
+        })
 
-        return x.df
+    return x.df
 
 def display(x):
     """
