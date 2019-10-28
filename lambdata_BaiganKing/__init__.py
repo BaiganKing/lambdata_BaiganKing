7a92eea5402450628cdb027c02ec03815d93fa82
Andrew <BaiganKing@users.noreply.github.com>
2019-10-28 15:31:47 -0600

    Added df_utils.py

--- lambdata_BaiganKing/__init__.py
+++ lambdata_BaiganKing/__init__.py
@@ -3,3 +3,6 @@ lambdata - a collection of data science functions
 """
 import pandas as pd
 import numpy as np
+
+def test(x):
+    return(x+1)
---
+++ lambdata_BaiganKing/df_utils.py
@@ -0,0 +1,25 @@
+"""
+functions to help work with dataframes
+"""
+import pandas as pd
+import numpy as np
+
+def date(x, col):
+    """
+    Function to split dates ("MM/DD/YYYY", etc.) into multiple columns
+    """
+    if x.df[col].dtype != 'datetime64[ns]':
+        x.df[col] = pd.to_datetime(x.df[col])
+
+     name = x.df[col].name
+        x.df[f'{name}_year'] = x.df[col].dt.year
+        x.df[f'{name}_month'] = x.df[col].dt.month
+        x.df[f'{name}_day'] = x.df[col].dt.day
+
+        x.df[f'{name}_month'] = x.df[f'{name}_month'].map({
+            1: 'January', 2: 'February', 3: 'March', 4: 'April', 5: 'May',
+            6: 'June', 7: 'July', 8: 'August', 9: 'September', 10: 'October',
+            11: 'November', 12: 'December'
+            })
+
+        return x.df
